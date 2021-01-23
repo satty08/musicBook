df2746a901a1f0304fb88ce7a269bb38d152ce70
Satyam <satyambhardawaj@gmail.com>
2021-01-23 10:12:43 +0530

    music

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import "./Home.css";
+import Album from './Music/Album';
 
 function Home() {
     return (
         <div className="home">
             <div className="home__music">
                 <h1 className="home__musicHeading">What to Listen</h1>
-                <button>Explore Music</button>
+                <button className="home__exploreMusic">Explore Music</button>
                 <div className="home__musicAlbums">
-
+                    <Album />
+                    <Album />
+                    <Album />
                 </div>
             </div>
             <div className="home__book">
                 <h1>What to Read</h1>
-                <button>Explore Library</button>
+                <button className="home__exploreBook">Explore Library</button>
                 <div className="home__bookLibrary">
                     
                 </div>
--- src/Music/Album.js
+++ src/Music/Album.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Button } from '@material-ui/core'
+import React from 'react';
+import './Album.css'
 
 function Album() {
     return (
         <div className="album">
-            
+            <h1>I'm the album</h1>
+            <Button className="album__button">Play Now</Button>
         </div>
     )
 }
