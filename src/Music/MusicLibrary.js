75aa33662efe60aae434e3c84c15068590288edc
Satyam <satyambhardawaj@gmail.com>
2021-01-26 11:26:31 +0530

    musicplayer setup

--- src/Music/MusicLibrary.js
+++ src/Music/MusicLibrary.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './MusicLibrary.css'
 import MusicSidebar from './MusicSidebar';
+import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import PlayCircleFilledWhiteOutlinedIcon from '@material-ui/icons/PlayCircleFilledWhiteOutlined';
+import ChevronRightOutlinedIcon from '@material-ui/icons/ChevronRightOutlined';
+import ChevronLeftOutlinedIcon from '@material-ui/icons/ChevronLeftOutlined';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import LoopIcon from '@material-ui/icons/Loop';
+import QueueMusicIcon from '@material-ui/icons/QueueMusic';
 
 function MusicLibrary() {
     return (
@@ -11,7 +18,26 @@ function MusicLibrary() {
                     <h1>I'm the main area</h1>
                 </div>
             </div>
-            <div className="musiclibrary__player"></div>
+            <div className="musiclibrary__player">
+                <div className="musiclibrary__playerleft">
+                    <FavoriteBorderOutlinedIcon />
+                </div>
+                <div className="musiclibrary__playermiddle">
+                    <div className="musiclibrary__playermiddleupper">
+                        <ShuffleIcon />
+                        <ChevronLeftOutlinedIcon />
+                        <PlayCircleFilledWhiteOutlinedIcon />
+                        <ChevronRightOutlinedIcon />
+                        <LoopIcon />
+                    </div>
+                    <div className="musiclibrary__playermiddlelower">
+                        
+                    </div>
+                </div>
+                <div className="musiclibrary__playerright">
+                    <QueueMusicIcon />
+                </div>
+            </div>
         </div>
     )
 }
--- src/Music/MusicSidebar.js
+++ src/Music/MusicSidebar.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import './MusicSidebar.css'
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
+import LibraryMusicOutlinedIcon from '@material-ui/icons/LibraryMusicOutlined';
+import QueueMusicOutlinedIcon from '@material-ui/icons/QueueMusicOutlined';
 
 function MusicSidebar() {
     return (
         <div className="musicsidebar">
-            <h1>I'm the sidear</h1>
+            <div className="musicsidebar__upper">
+                <h2 className="musicsidebar__content">
+                    <HomeOutlinedIcon />
+                    <span>Home</span>
+                </h2>
+                <h2 className="musicsidebar__content">
+                    <LibraryMusicOutlinedIcon />
+                    <span>Library</span>
+                </h2>
+                <h2 className="musicsidebar__content">
+                    <QueueMusicOutlinedIcon />
+                    <span>Collection</span>
+                </h2>
+            </div>
+            <div className="musicsidebar__lower">
+                <p>Install App</p>
+            </div>
         </div>
     )
 }
